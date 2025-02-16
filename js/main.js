a2dfb9961dea35a7a891664283a5c87c85baac49
ProDezzzz <edward.rh.nandez@gmail.com>
2025-02-16 11:19:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 document.getElementById("convertBtn").addEventListener("click", function() { 
 const robuxAmount = document.getElementById("robuxAmount").value; 
-const exchangeRate = 180; // Rp 18,000 / 100 robux = Rp 180 per robux 
+const exchangeRate = 160; // Rp 16,000 / 100 robux = Rp 160 per robux 
 const rupiahAmount = robuxAmount * exchangeRate; 
 const formattedRupiah = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(rupiahAmount); 
 //const formattedRupiah = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(rupiahAmount); 
